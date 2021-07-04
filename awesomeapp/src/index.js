233536afa7ff70bf6755253517b11214cda143f0
saurav <saurav12797@gmail.com>
2021-07-04 12:53:51 +0530

    Df

---
+++ awesomeapp/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+ 
+   <>
+   <h1>Saurav Netflix Pickup</h1>
+   <p> How are You</p>
+   <ol>
+   <li>Dark</li>
+   <li>Holy love</li>
+   <li>Mind</li>
+   <li>Game</li>
+
+
+   </ol>
+   </>,
+   document.getElementById('root')
+
+
+);
